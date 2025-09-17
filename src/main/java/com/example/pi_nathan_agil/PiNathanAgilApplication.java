87122933f87464c4e3fe94c095518fee8e1aa256
nadapebe <nathandpb@al.insper.edu.br>
2025-09-17 10:46:03 -0300

    0.1

---
+++ src/main/java/com/example/pi_nathan_agil/PiNathanAgilApplication.java
@@ -0,0 +1,13 @@
+package com.example.pi_nathan_agil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiNathanAgilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiNathanAgilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pi_nathan_agil/PiNathanAgilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pi_nathan_agil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiNathanAgilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
